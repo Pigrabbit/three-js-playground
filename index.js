184090ffcf86d3bbd5c9739ceebe45526368c178
Pigrabbit <donghyuk1003@gmail.com>
2020-10-05 15:46:52 +0900

    feat: create cube with line edge and make it move

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 import * as THREE from '../node_modules/three/build/three.module.js'
 
-let camera, scene, renderer
+let camera, scene, renderer, cube
+let step = 0.05
+
+function buildCube() {
+  let geometry = new THREE.BoxGeometry(1, 1, 1)
+  let material = new THREE.MeshBasicMaterial({ color: 0xff9631 })
+  cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+}
+
+function buildEdge() {
+  let geometry = new THREE.EdgesGeometry(cube.geometry)
+  let material = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 2 })
+  let edges = new THREE.LineSegments(geometry, material)
+  cube.add(edges)
+}
 
 function init() {
   scene = new THREE.Scene()
   scene.background = new THREE.Color(0xababab)
 
   camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000)
-  camera.position.z = 5
+  camera.position.z = 8
+  camera.position.y = 1
 
-  renderer = new THREE.WebGLRenderer();
+  buildCube()
+  buildEdge()
+
+  renderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth, window.innerHeight)
 
   document.body.appendChild(renderer.domElement)
 }
 
 function mainLoop() {
+  cube.position.x += step
+
+  if (cube.position.x <= -3 || cube.position.x >= 3) {
+    step *= -1
+  }
   renderer.render(scene, camera)
   requestAnimationFrame(mainLoop)
 }
