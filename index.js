93901d48b13170c960e028db68c9cf65507f813f
Pigrabbit <donghyuk1003@gmail.com>
2020-10-05 15:07:01 +0900

    feat: paint background scene

---
+++ index.js
@@ -0,0 +1,24 @@
+import * as THREE from '../node_modules/three/build/three.module.js'
+
+let camera, scene, renderer
+
+function init() {
+  scene = new THREE.Scene()
+  scene.background = new THREE.Color(0xababab)
+
+  camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000)
+  camera.position.z = 5
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  document.body.appendChild(renderer.domElement)
+}
+
+function mainLoop() {
+  renderer.render(scene, camera)
+  requestAnimationFrame(mainLoop)
+}
+
+init()
+mainLoop()
